404074a65cea5d5007b3f7df6ba81b5cd9874460
LucaCogo <lucacogo997@gmail.com>
2022-07-06 16:41:09 +0200

    Created BasicVSRAFT

--- mmedit/models/backbones/__init__.py
+++ mmedit/models/backbones/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .sr_backbones import BasicVSRPlusPlus
-
-__all__ = ['BasicVSRPlusPlus']
+from .sr_backbones import BasicVSRAFT
+__all__ = ['BasicVSRPlusPlus', 'BasicVSRAFT']
--- mmedit/models/backbones/sr_backbones/__init__.py
+++ mmedit/models/backbones/sr_backbones/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .basicvsr_net import BasicVSRNet
 from .basicvsr_pp import BasicVSRPlusPlus
+from .basicvsraft import BasicVSRAFT
 
-__all__ = ['BasicVSRNet', 'BasicVSRPlusPlus']
+__all__ = ['BasicVSRNet', 'BasicVSRPlusPlus', 'BasicVSRAFT']
