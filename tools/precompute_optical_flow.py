a0f3f7c2bb558d693b746819cc36920e2ad338d5
LucaCogo <lucacogo997@gmail.com>
2022-11-03 16:10:54 +0100

    Corrections to precompute_optical_flow.py

--- tools/precompute_optical_flow.py
+++ tools/precompute_optical_flow.py
@@ -2,11 +2,10 @@
 
 import sys
 import os
-
-sys.path.append(os.getcwd)
-
+import torch
 import argparse
 import glob
+import numpy as np
 from PIL import Image
 from mmedit.models.RAFT.raft import RAFT
 
@@ -23,52 +22,52 @@ def load_image(imfile):
 
 
 def compute_flows(args):
+    model = torch.nn.DataParallel(RAFT(args))
+    model.load_state_dict(torch.load(args.model, map_location= torch.device(DEVICE)))
 
-	model = torch.nn.DataParallel(RAFT(args))
-	model.load_state_dict(torch.load(args.model, map_location= torch.device(DEVICE)))
+    
+    model = model.eval()
 
-	model = model.module()
-	model.eval()
+    input_folder = "lq_sequences_train"
+    output_folder = "lq_sequences_train_of"
 
-	input_folder = "lq_sequences_train"
-	output_folder = "lq_sequences_train_of"
+    imagespath = os.path.join(args.path, input_folder)
+    targetpath = os.path.join(args.path, output_folder)
 
-	imagespath = os.path.join(args.path, input_folder)
-	targetpath = os.path.join(args.path, output_folder)
+    if not os.path.exists(targetpath):
+      os.mkdir(targetpath)
 
-	if not os.path.exists(targetpath):
-		os.mkdir(targetpath)
+    with torch.no_grad():
+      for d in os.listdir(imagespath):
+        sequencepath = os.path.join(imagespath, d)
+        print(f"Computing optical flow for sequence: {d}")
 
-	with torch.no_grad():
-		for d in os.listdir(imagespath):
-			sequencepath = os.path.join(imagespath, d)
-			print(f"Computing optical flow for sequence: {d}")
+        image_filenames = glob.glob(os.path.join(sequencepath, '*.png')) + \
+                    glob.glob(os.path.join(sequencepath, '*.jpg'))
 
-			image_filenames = glob.glob(os.path.join(sequencepath, '*.png')) + \
-                 glob.glob(os.path.join(sequencepath, '*.jpg'))
+        image_filenames = sorted(image_filenames)
 
-			image_filenames = sorted(image_filenames)
+              
+        frames = []
+        for imfile in image_filenames:
+          frames.append(load_image(imfile))
 
-            
-			frames = []
-			for imfile in image_filenames:
-			        frames.append(load_image(imfile))
-			frames = torch.cat(frames).to(DEVICE)
+        frames = torch.cat(frames).to(DEVICE)
 
-			padder = InputPadder(frames.shape)
-			frames = padder.pad(frames)[0]
+        padder = InputPadder(frames.shape)
+        frames = padder.pad(frames)[0]
 
-			frames_batch1 = frames[:-1]
-			frames_batch2 = frames[1:]
+        frames_batch1 = frames[:-1]
+        frames_batch2 = frames[1:]
 
-			_, flow_out = model(frames_batch1, frames_batch2, iters=32, test_mode = True)
+        _, flow_out = model(frames_batch1, frames_batch2, iters=32, test_mode = True)
 
 
-			filename = os.path.join(targetpath,d) + ".flo"
-			fileObj = open(filename, 'wb')
-			flow_out.cpu().numpy().tofile(fileObj)
+        filename = os.path.join(targetpath,d) + ".flo"
+        fileObj = open(filename, 'wb')
+        flow_out.cpu().numpy().tofile(fileObj)
 
-			fileObj.close()
+        fileObj.close()
 
 
 
@@ -82,6 +81,5 @@ if __name__ == '__main__':
 	parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
 	parser.add_argument('--alternate_corr', action='store_true', help='use efficient correlation implementation')
 	
-	
+	args = parser.parse_args()
 	compute_flows(args)
-	
