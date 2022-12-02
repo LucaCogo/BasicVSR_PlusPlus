fcef50d6795ca3629d4382449c51599bc8a332ad
LucaCogo <lucacogo997@gmail.com>
2022-12-02 15:40:05 +0100

    Add LiteFlowNet

--- mmedit/models/backbones/__init__.py
+++ mmedit/models/backbones/__init__.py
@@ -4,5 +4,7 @@ from .sr_backbones import BasicVSRAFT
 from .sr_backbones import BasicVSR_Farneback
 from .sr_backbones import BasicVSRSPyNet_Original
 from .sr_backbones import BasicVSRAFT_precomp
+from .sr_backbones import BasicVSR_LFN
 
-__all__ = ['BasicVSRPlusPlus', 'BasicVSRAFT', 'BasicVSR_Farneback', 'BasicVSRSPyNet_Original', 'BasicVSRAFT_precomp']
+
+__all__ = ['BasicVSRPlusPlus', 'BasicVSRAFT', 'BasicVSR_Farneback', 'BasicVSRSPyNet_Original', 'BasicVSRAFT_precomp', 'BasicVSR_LFN']
--- mmedit/models/backbones/sr_backbones/__init__.py
+++ mmedit/models/backbones/sr_backbones/__init__.py
@@ -5,5 +5,6 @@ from .basicvsraft import BasicVSRAFT
 from .basicvsr_farneback import BasicVSR_Farneback
 from .basicvsr_spynet_original import BasicVSRSPyNet_Original
 from .basicvsraft_precomp import BasicVSRAFT_precomp
+from .basicvsr_lfn import BasicVSR_LFN
 
-__all__ = ['BasicVSRNet', 'BasicVSRPlusPlus', 'BasicVSRAFT', 'BasicVSRAFT_precomp','BasicVSR_Farneback', 'BasicVSRSPyNet_Original']
+__all__ = ['BasicVSRNet', 'BasicVSRPlusPlus', 'BasicVSRAFT', 'BasicVSRAFT_precomp','BasicVSR_Farneback', 'BasicVSRSPyNet_Original', 'BasicVSR_LFN']
